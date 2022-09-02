d284f01bc7e8e26bfbf64b6810b788ef0e9e21e0
RodericoNeto <rodericocrneto@gmail.com>
2022-09-01 21:02:52 -0300

    form react e formdata

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import './App.css';
 import Book from './components/Book';
+import FormBook from './pages/FormBook';
 
 export default function App() {
 
-  // let livros = [
-  //   { id: 1, title: "O senhor dos aneis" },
-  //   { id: 2, title: 'O poderoso chefão' },
-  //   { id: 3, title: 'Carrie, A estranha' },
-  //   { id: 4, title: 'Carrie, A estranha' },
-  //   { id: 5, title: 'Platoon' }
-  // ]
-
-  // return (
-  //   <>
-  //     <h1>Livros</h1>
-  //     {
-  //       livros.map(element => {
-  //         console.log(element.title)
-  //           return <Book key = { element.id } title = { element.title } />
-  // })
-  //     }
-  //   </>
-  // )
-
-
-let livros = ["O senhor dos aneis", 'O poderoso chefão','Carrie, A estranha', 'Platoon','Carrie, A estranha']
-
+  let livros = [
+    { id: 1, title: "O senhor dos aneis" },
+    { id: 2, title: 'O poderoso chefão' },
+    { id: 3, title: 'Carrie, A estranha' },
+    { id: 4, title: 'Carrie, A estranha' },
+    { id: 5, title: 'Platoon' }
+  ]
 
   return (
     <div>
+      <FormBook />
       <h1>Livros</h1>
-        {
-          livros.map(element => <Book key={element} title={element}/>)
-        }
+
+      {
+        livros.map(element => {
+          console.log(element.title)
+          return <Book key={element.id} title={element.title} />
+        })
+      }
     </div>
-  );
-}
+  )
+};
+
+
